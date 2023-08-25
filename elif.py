c318a60d2b685a90dc9b9aac4f12ddd66d649958
caedufreitas <Ti.carlosfreitas@outlook.com>
2023-08-25 10:56:12 -0300

    laco de repeticao aninhada

---
+++ elif.py
@@ -0,0 +1,9 @@
+﻿chave = 7
+numero = input('Digite um Numero: \n')
+valor = int(numero)
+if (chave == valor):
+    print('Voçê acertou!')
+elif (valor > chave):
+    print('Voçê errou! O seu chute foi maior que o numero secreto!')
+elif (valor < chave):
+    print('Voçê errou! O seu chute foi menor que o numero secreto!')
