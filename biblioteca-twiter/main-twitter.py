0fa709046666af0a2a7ffc85e7bb4546d00c1415
caedufreitas <Ti.carlosfreitas@outlook.com>
2023-08-25 10:48:21 -0300

    biblioteca twint

---
+++ biblioteca-twiter/main-twitter.py
@@ -0,0 +1,23 @@
+'''
+Falta criar uma documentação dos requisitos funcionais do sistema.
+Dá um import, passa os parametros e fica pythonico.....
+E assim você cria uma classe para reutilizar ao dia a dia.
+'''
+from twitter import Twitter
+
+consumer_key = str(input('Consumer Key: '))
+consumer_secret = str(input('Consumer Secret: '))
+token_key = str(input('Token Key: '))
+token_secret = str(input('Token Secret: '))
+# criando um objeto do tipo twitter, passando a conexao
+twitter = Twitter(consumer_key, consumer_secret, token_key, token_secret)
+
+# postando pelo metodo, obtendo resposta
+# resposta = twitter.post_twitter('Vamos testar nossa lib')
+# print(resposta)
+# pesquisando
+pesquisa = twitter.search_twitter('solyd', 'pt')
+# print(pesquisa)
+for resultado in pesquisa:
+    print(resultado['text'])
+    print(resultado['user']['screen_name'])
