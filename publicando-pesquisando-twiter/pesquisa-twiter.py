6b5b438b3872d99f3af31ddc86a6548ceb655b38
caedufreitas <Ti.carlosfreitas@outlook.com>
2023-08-25 11:46:05 -0300

    pesquisando e publicando no twiter

---
+++ publicando-pesquisando-twiter/pesquisa-twiter.py
@@ -0,0 +1,10 @@
+'''
+streaming - tempo real
+API - twiter
+Mineração de dados, ou monitoramento do que as
+pessoas estão pensando em tempo real sobre aquela empresa
+Oauth2 - um padrao para internet de authenticação.
+'''
+
+
+
